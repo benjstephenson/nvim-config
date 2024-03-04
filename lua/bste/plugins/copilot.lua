93aa4c0b08893f9ba372528e65f33b0265e1bcbd
Benji Stephenson <bjames.stephenson@gmail.com>
2024-03-04 17:54:36 +0000

    chore: package update, disable copilot

--- lua/bste/plugins/copilot.lua
+++ lua/bste/plugins/copilot.lua
@@ -2,10 +2,10 @@ return {
     --"github/copilot.vim",
     "zbirenbaum/copilot.lua",
     config = function()
-        require("copilot").setup({
-            suggestion = {
-                auto_trigger = true,
-            },
-        })
+        --require("copilot").setup({
+        --    suggestion = {
+        --        auto_trigger = true,
+        --    },
+        --})
     end,
 }
